5e978a8a3aec944cfe817ded3e6f049eb62edbee
Zack-19 <jz_safa@esi.dz>
2026-08-16 20:34:38 +0100

    adding user data

---
+++ lib/features/auth/model/user_model.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+class UserModel {
+  final String displayName;
+  final String username;
+  final String email;
+  final String profilePic;
+  final String subscriptions;
+  final String videos;
+  final String userId;
+  final String description;
+  final String type;
+
+  UserModel({
+    required this.displayName, 
+    required this.username, 
+    required this.email, 
+    required this.profilePic, 
+    required this.subscriptions, 
+    required this.videos, 
+    required this.userId, 
+    required this.description, 
+    required this.type});
+
+   factory UserModel.fromMap(Map<String,dynamic> map){
+    return UserModel(
+      displayName: map['displayName'] as String,
+      username: map['username'] as String,
+      email: map['email'] as String,
+      profilePic: map['profilePic'] as String,
+      subscriptions: map['subscriptions'] as String,
+      videos: map['videos'] as String,
+      userId: map['userId'] as String,
+      description: map['description'] as String,
+      type: map['type'] as String,
+
+    );
+  } 
+
+  String toJson()=>json.encode(toMap());
+  factory UserModel.fromJson(String source)=>UserModel.fromMap(json.decoder(source)as )
+}
+
+ 
