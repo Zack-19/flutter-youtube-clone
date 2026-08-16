29235ad2001825f0952a688a68af8cedc549fdde
Zack-19 <jz_safa@esi.dz>
2026-08-16 17:17:47 +0100

    google auth web/mobile

--- lib/features/auth/repository/auth_service.dart
+++ lib/features/auth/repository/auth_service.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/foundation.dart';
 
 final authServiceProvider =  Provider(
   (ref)=>AuthService(auth: FirebaseAuth.instance,googleSignIn: GoogleSignIn.instance ,),
@@ -15,7 +16,8 @@ class AuthService {
     required this.googleSignIn,
   });
 
-  signInWithGoogle() async{
+  /*signInWithGoogle() async{
+    //final GoogleSignIn googleSignIn = GoogleSignIn(clientId : '99990800267-j3d110mi656hgq0p9qq9p34i0ccrbfh9.apps.googleusercontent.com');
     final GoogleSignIn googleSignIn = GoogleSignIn.instance;
     await googleSignIn.initialize();
     final user = await googleSignIn.authenticate();
@@ -29,6 +31,26 @@ class AuthService {
 
 
 
+  }*/
+  Future<UserCredential?> signInWithGoogle() async {
+    if (kIsWeb) {
+      final GoogleAuthProvider googleProvider = GoogleAuthProvider();
+
+      return await auth.signInWithPopup(googleProvider);
+    }
+
+    // Android / iOS
+    await googleSignIn.initialize();
+
+    final user = await googleSignIn.authenticate();
+
+    final googleAuth = user.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      idToken: googleAuth.idToken,
+    );
+
+    return await auth.signInWithCredential(credential);
   }
 
 
