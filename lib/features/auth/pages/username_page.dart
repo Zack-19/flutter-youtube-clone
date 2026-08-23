e14d23629c957cc7ecef2b23e856586b12df9c79
Zack-19 <jz_safa@esi.dz>
2026-08-23 18:08:38 +0100

    username validation logic

--- lib/features/auth/pages/username_page.dart
+++ lib/features/auth/pages/username_page.dart
@@ -1,9 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:youtube_clone/cores/widgets/flat_button.dart';
+import 'package:youtube_clone/features/auth/repository/user_data_service.dart';
 
-class UsernamePage extends StatelessWidget {
-  const UsernamePage({super.key});
+final formKey = GlobalKey<FormState>();
 
+class UsernamePage extends ConsumerStatefulWidget {
+  final String displayName;
+  final String profilePic;
+  final String email;
+
+  const UsernamePage({
+    required this.displayName,
+    required this.profilePic,
+    required this.email,
+
+  });
+  @override
+  ConsumerState<UsernamePage> createState() => _UsernamePageState();
+}
+
+
+
+class _UsernamePageState extends ConsumerState<UsernamePage> {
+  final TextEditingController usernameController = TextEditingController();
+  bool isValid = true;
+  void validateUsername()async{
+    final usersMap = await FirebaseFirestore.instance.collection("users").get();
+    final users = usersMap.docs.map((user)=>user).toList();
+    String? targetedUsername;
+    for (var user in users){
+      if(usernameController.text==user.data()["username"]){
+        targetedUsername = user.data()["username"];
+        isValid = false;
+        setState((){});
+      }
+      if(usernameController.text!=targetedUsername){
+        isValid = true;
+        setState(() {});
+      }
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,9 +57,18 @@ class UsernamePage extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(left :  15 , right: 15),
               child: TextFormField(
+                onChanged: (username){
+                  validateUsername();
+                },
+                autovalidateMode: AutovalidateMode.always,
+                validator: (username){
+                  isValid ? null : "username already taken ";
+                },
+                key: formKey,
+                controller: usernameController,
                 decoration: InputDecoration(
-                  suffixIcon: Icon(Icons.verified_user_rounded),
-                  suffixIconColor: Colors.green,
+                  suffixIcon: isValid ?  Icon(Icons.verified_user_rounded): Icon(Icons.cancel),
+                  suffixIconColor: isValid ? Colors.green : Colors.red,
                   hintText: "insert user name",
                   border: const OutlineInputBorder(
                     borderSide: BorderSide(
@@ -43,7 +91,18 @@ class UsernamePage extends StatelessWidget {
             const Spacer(),
             Padding(
               padding: const EdgeInsets.only(bottom: 30,left:15,right:30),
-              child: FlatButton(text:"continue", onPressed: (){}, colour: Colors.green),
+              child: FlatButton(
+                text:"continue", 
+                onPressed: () async{
+                  isValid ? 
+                  await ref.read(userDataServiceProvider).addUserDataToFirestore(
+                    displayName: widget.displayName, 
+                    username: usernameController.text, 
+                    email:widget.email, 
+                    profilePic: widget.profilePic, 
+                    description: "") : null;
+                }, 
+                colour: isValid ? Colors.green : Colors.green.shade100),
             ),
           ],
         ) ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:youtube_clone/cores/screens/loader.dart';
 import 'package:youtube_clone/features/auth/pages/login_page.dart';
 import 'package:youtube_clone/features/auth/pages/username_page.dart';
 import 'package:youtube_clone/firebase_options.dart';
+import 'package:youtube_clone/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,8 +31,20 @@ class MyApp extends ConsumerWidget {
         builder: (context,snapshot){
           if(!snapshot.hasData){
             return LoginPage();
+          }else if (snapshot.connectionState == ConnectionState.waiting){
+            return Loader();
           }
-          return UsernamePage();
+          return StreamBuilder(
+            stream: FirebaseFirestore.instance.collection("users").doc(FirebaseAuth.instance.currentUser!.uid).snapshots(), 
+            builder: (context,snapshot){
+              final user = FirebaseAuth.instance.currentUser;
+              if(!snapshot.hasData|| !snapshot.data!.exists){
+                return UsernamePage(displayName: user!.displayName!, profilePic:user.photoURL!, email: user.email!);
+              }else if (snapshot.connectionState == ConnectionState.waiting){
+            return Loader();
+          }
+              return HomePage();
+            });
         }
         ),
     );
